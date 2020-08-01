6fe8b78a955a9236f3fc5b8f97108db2f1e6fe15
Meatball <mainmeatball1@gmail.com>
2020-08-01 09:47:36 +0300

    Improve strategy according to use of event.key instead of event.keyCode

--- Project 10/index.js
+++ Project 10/index.js
@@ -2,7 +2,10 @@ const input = document.getElementById('input');
 let result;
 let operation = '';
 let deleteIfEnter = false;
-let clearMemory = false;
+let shouldClearMemory = false;
+
+const terminalOperations = ['','+','-','*','/','=','Enter'];
+const intermediateOperations = ['Backspace', 'c'];
 
 const operations = {
 	'' : (value) => sumResult(value),
@@ -10,60 +13,37 @@ const operations = {
 	'-': (value) => sumResult(-value),
 	'*': (value) => multiplyResult(value),
 	'/': (value) => divideResult(value),
-	'=': (value) => showResult(value)
+	'=': (value) => showResult(value),
+	'Enter': (value) => showResult(value),
+	'Backspace': () => backspace(),
+	'c': () => clearInput()
 }
 
 function onload() {
-	document.addEventListener("keydown", keyDownInput, false);
+	document.addEventListener("keydown", keyDownInput);
 }
 
 function keyDownInput(e) {
-  let keyCode = e.keyCode;
-  let shift = e.shiftKey;
-  if (shift == false && keyCode >= 48 && keyCode <= 57) {
-    addNumberUI(keyCode - 48);
-    return;
-  }
-  if (keyCode == 191) {
-  	operate('/');
-  	return;
-  }
-  if (shift == true && keyCode == 58) {
-  	operate('*');
-  	return;
-  }
-  if (keyCode == 189) {
-  	operate('-');
-  	return;
-  }
-  if (shift == true && keyCode == 187) {
-  	operate('+');
-  	return;
-  }
-  if (keyCode == 187 || keyCode == 13) {
-  	operate('=');
-  	return;
-  }
-  if (keyCode == 8) {
-  	backspace();
-  	return;
-  }
-  if (keyCode == 67) {
-  	clearInput();
-  	return;
-  }
+	let key = e.key;
+	if (key >= '0' && key <= '9') {
+		addNumberUI(key);
+		return;
+	}
+	if (isTerminalOperation(key)) {
+		operate(key);
+		return;
+    }
+    if (isIntermediateOperation(key)) {
+        operations[key](input.value);
+    }
 }
 
 function clearInput() {
 	input.value = '';
-	if (!clearMemory) {
-		clearMemory = true;
-	} else {
-		clrMemory();
-	}
+	shouldClearMemory ? clearMemory() : shouldClearMemory = true;
 }
 
-function clrMemory() {
+function clearMemory() {
 	result = 0;
 	operation = '';
 }
@@ -74,7 +54,7 @@ function backspace() {
 }
 
 function operate(op) {
-	clearMemory = false;
+	shouldClearMemory = false;
 	operations[operation](input.value);
 	operation = op;
 	deleteIfEnter = true;
@@ -125,4 +105,12 @@ function showResult(value) {
 
 function updateInput(value) {
 	input.value = value;
+}
+
+function isTerminalOperation(key) {
+	return terminalOperations.includes(key);
+}
+
+function isIntermediateOperation(key) {
+	return intermediateOperations.includes(key);
 }
