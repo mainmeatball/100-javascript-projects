fb0d9eb748f2a1574bd2e3fc559e2e4b5331f4be
Meatball <mainmeatball1@gmail.com>
2020-08-02 17:35:04 +0300

    Project 11: incapsulate tip results

--- Project 11/index.js
+++ Project 11/index.js
@@ -13,11 +13,25 @@ class InputEntity {
 	assignVisibility() {
 		this.isValid() ? this.validation.hide() : this.validation.show();
 	}
+
+	getValue() {
+		return +this.input.value;
+	}
+}
+
+class TipResult {
+	constructor(tip, total, perPerson) {
+		this.tip = tip;
+		this.total = total;
+		this.perPerson = perPerson;
+	}
 }
 
 const billAmount = new InputEntity($('#bill-amount')[0], $('#amount-validation'));
 const personNumber = new InputEntity($('#person-number')[0], $('#person-validation'));
 const tipPercent = new InputEntity($('#tip-percent')[0], $('#tip-validation'));
+
+const inputs = [billAmount, personNumber, tipPercent];
 
 const tipAmount = $('#tip-amount')[0];
 const totalAmount = $('#total-amount')[0];
@@ -34,9 +48,9 @@ info.hide();
 
 const UPLOAD_ANIMATION_DURATION = 3000;
 
-function calculateTip() {
+function process() {
+	assignVisibility();
 	if (!validateInputs()) {
-		assignVisibility();
 		result.hide();
 		return;
 	}
@@ -50,23 +64,28 @@ function calculateTip() {
        info.show();
 	}, UPLOAD_ANIMATION_DURATION);
 
-	const bill = +billAmount.input.value;
-	const people = +personNumber.input.value;
-	const tip = +tipPercent.input.value;
+	const bill = billAmount.getValue();
+	const people = personNumber.getValue();
+	const tip = tipPercent.getValue();
 	
-	const tipValue = bill * tip;
-	const total = bill + tipValue;
-	const perPerson = total / people;
-
-	tipAmount.textContent = tipValue;
-	totalAmount.textContent = total;
-	tipPerPerson.textContent = perPerson;
+	const tipResult = calculateTips(bill, people, tip);
+	
+	tipAmount.textContent = tipResult.tip;
+	totalAmount.textContent = tipResult.total;
+	tipPerPerson.textContent = tipResult.perPerson;
 }
 
 function validateInputs() {
-	return [billAmount, personNumber, tipPercent].every(el => el.isValid());
+	return inputs.every(el => el.isValid());
 }
 
 function assignVisibility() {
-	[billAmount, personNumber, tipPercent].forEach(el => el.assignVisibility());
+	inputs.forEach(el => el.assignVisibility());
 }
+
+function calculateTips(bill, people, tip) {
+	const localTip = bill * tip;
+	const total =  bill + localTip;
+	const perPerson = total / people;
+	return new TipResult(localTip, total, perPerson);
+}	
