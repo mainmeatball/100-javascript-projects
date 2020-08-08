0020a78067a89501b6bf2f065e02f7e1620f37dd
Meatball <mainmeatball1@gmail.com>
2020-08-08 20:57:33 +0300

    Project 13: remove types when initializing variable

--- Project 13/index.ts
+++ Project 13/index.ts
@@ -2,16 +2,16 @@ import {Grid} from "./entity/grid";
 import {Shop} from "./entity/shop";
 import {Page} from "./entity/page";
 
-const shop: Shop = new Shop(
+const shop = new Shop(
     <HTMLParagraphElement>document.getElementById('quantity'),
     <HTMLParagraphElement[]>Array.from(document.getElementsByClassName('amount')),
     <HTMLDivElement>document.getElementById('shopping-window'),
     <HTMLTemplateElement>document.getElementById('cart-item-template')
 );
 
-const grid: Grid = new Grid(
+const grid = new Grid(
     <HTMLDivElement>document.getElementById('flex-grid'),
     <HTMLTemplateElement>document.getElementById('frame-template')
 );
 
-export const page: Page = new Page(shop, grid);
+export const page = new Page(shop, grid);
